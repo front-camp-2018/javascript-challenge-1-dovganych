73522a9129d0b0381766feb64f88692ee9e67c14
dovganych <misha.dovganych@gmail.com>
2018-10-29 17:45:00 +0200

    add solutions

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const str = num.toString();
+  const regex = new RegExp('.{1,' + str.length / 2 + '}', 'g');
+  if ((str.length & 1)) {
+    return num * 2;
+  }
+  return str.match(regex).every((val, i, arr) => val === arr[0] ) ? num : num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable-next-line */
+const convert = item => {
+  return item.split('').map(item => +item).reduce((a, b) => a + b);
+};
 export const sortByWeight = arr => {
-  // your logic here...
+  return arr.sort((a,b) => {
+    return convert(a) - convert(b);
+  });
 };
